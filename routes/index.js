aa1344446a81d8dfcf2ff824ad878bc2871868f4
ram kumar <rammaniyari@gmail.com>
2023-09-10 00:09:13 +0530

    sing in router is created

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
-const {signUp} = require("../controllers/user_controller");
+const {signUp, signIn} = require("../controllers/user_controller");
 const {home} = require("../controllers/home_controller");
 
 router.get("/",home);
 router.get("/users/sign-up",signUp);
+router.get("/users/sign-in",signIn)
 
 
 
