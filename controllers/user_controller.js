05f0b9564674eb29902a34da4795c5c060bbe613
ram kumar <rammaniyari@gmail.com>
2023-09-10 00:06:32 +0530

    a function added to home controller

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -14,3 +14,22 @@ module.exports.signUp = async (req, res) => {
         return res.redirect('back');
     }
 }
+
+
+// This function is for providing the 7days date, which will be displayed afte the habit is created.
+function getOneWeekDate(){
+    let months = ["","Jan", "Feb", "March", "Apr", "May", "June", "Jul", "Aug", "Sept", "Oct", "Nov", "Dec"];
+    let dates = [];
+    for(let i = 6; i>=0 ; i--){
+        const currentDate = new Date();
+        currentDate.setDate(currentDate.getDate() - i);
+        let mm = currentDate.getMonth()+1;
+        mm = months[mm];
+        let dd = currentDate.getDate();
+        if (dd < 10) dd = '0' + dd;
+        dates.push(mm +" " +dd);
+    }
+    return dates;
+}
+
+
