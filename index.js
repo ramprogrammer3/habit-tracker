5ad37c94ff2cc239eaf1e9c81c21af0ecdcf9597
ram kumar <rammaniyari@gmail.com>
2023-09-09 23:27:58 +0530

    view engine ejs setup

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
 const express =require('express');
 require("dotenv").config();
+
 const connect = require("./config/dbConnect");
+
+const expressLayouts = require('express-ejs-layouts');
+const bodyParser = require('body-parser');
+
 const app = express();
 
 const port = process.env.port || 8080;
 
+
+
 // db connection
 connect();
+
+// layouts for ejs
+app.use(expressLayouts);
+app.use(bodyParser.urlencoded({extended:false}));
+
+
+// set up the view engine
+app.set('view engine', 'ejs');
+app.set('views', './views');
+app.use(express.static('./assets')); 
 
 
 
